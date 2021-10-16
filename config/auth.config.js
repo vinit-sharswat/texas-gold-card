74c847d0e55ce8f97b8936bdbabb892bcbaebe0e
VINIT SHARSWAT <vsharswa@in.ibm.com>
2021-10-16 12:47:42 +0530

    Added refreshAccessToken toDo

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,4 +1,6 @@
 require('dotenv').config({ silent: true });
 module.exports = {
-    secret: process.env.AUTH_SECRET
+    secret: process.env.AUTH_SECRET,
+    jwtExpiration: 3600,           // 1 hour
+    jwtRefreshExpiration: 86400,   // 24 hours
 };
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -99,7 +99,7 @@ exports.signin = (req, res) => {
                 authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
             }
             res.status(200).send({
-                id: user._id,
+                // id: user._id,
                 username: user.username,
                 email: user.email,
                 roles: authorities,
