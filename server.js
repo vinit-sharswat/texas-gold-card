6ed0fd54f605aba8e84f67b909504b433e85874e
VINIT SHARSWAT <vsharswa@in.ibm.com>
2021-10-21 14:48:33 +0530

    Updated the code begin command

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ require('./routes/profile.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`TGC Backend Server is running on port ${PORT}.`);
 });
 
 function initial() {
